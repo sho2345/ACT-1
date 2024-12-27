8b18def588f8221d525a370bfa6452b3cd189b83
Shogo Iwakata <happyganpen@fuji.waseda.jp>
2024-12-27 14:16:10 +0000

    add detr_directory

--- training/policy.py
+++ training/policy.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 import torchvision.transforms as transforms
 
+# from detr.main import build_ACT_model_and_optimizer, build_CNNMLP_model_and_optimizer
 from detr.main import build_ACT_model_and_optimizer, build_CNNMLP_model_and_optimizer
 import IPython
 e = IPython.embed
